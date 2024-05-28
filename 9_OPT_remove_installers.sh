dc93235ef6bcf94563701094780754bb25f13dce
Haraldo Filho <haraldo.filho@gmail.com>
2024-05-28 14:28:59 -0300

    Edited 9_OPT_remove_installers.sh

--- 9_OPT_remove_installers.sh
+++ 9_OPT_remove_installers.sh
@@ -2,7 +2,7 @@
 
 sudo pwd > /dev/null
 
-sudo rm /usr/share/applications/*.rpm
-sudo rm /usr/share/applications/*.run
-sudo rm /usr/share/applications/*.sh
+sudo rm /home/admin/Installers/packages/*.rpm
+sudo rm /home/admin/Installers/runfiles/*.run
+sudo rm /home/admin/Installers/runfiles/*.sh
 sudo rm /home/admin/Installers/tarfiles/*.tar.gz
